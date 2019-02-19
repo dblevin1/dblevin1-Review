04db556bb04b995ecd3cccda5f77c9fd22d57d94
dblevin1 <dblevin1@asu.edu>
2019-02-18 21:48:16 -0700

    Add File Banner

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,9 +1,17 @@
 package main.java;
 
+/*
+        File: Item.java
+        Author:	@author amehlhas, adalvi1
+        Date: 2/18/2019
+
+        Description: Class to hold a key value
+*/
+
 public final class Item {
         public int key;
         //constructor
-        public Item(int keyValue) { 
+        public Item(int keyValue) {
                 key = keyValue;
         }
         // further components and methods
